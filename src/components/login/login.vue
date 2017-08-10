1757f58f1b22463a01badf55b289a1cfc926cfcc
c <jscc2333@163.com>
2017-08-10 17:54:10 +0800

    修复浏览器回退用户不注销bug

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -35,210 +35,220 @@
 </template>
 
 <script type="text/ecmascript-6">
-import router from '../../router';
-import { loadFromLocal, saveToLocal } from '../../common/js/store.js';
+  import router from '../../router';
+  import { loadFromLocal, saveToLocal } from '../../common/js/store.js';
 
-const OPTION_TYPE_REGSIT = true;
+  const OPTION_TYPE_REGSIT = true;
 
-export default {
-  data() {
-    return {
-      name: '',
-      password: '',
-      cPassword: '',
-      message: '',
-      regist: false,
-      optionType: OPTION_TYPE_REGSIT,
-      flag: true,
-      submitNumber: 0
-    };
-  },
-  watch: {
-    // 监测name的值
-    name: function () {
-      this.password = '';
-      let password = loadFromLocal(this.name);
-      if (password) {
-        // 若本地缓存存在密码，则自动补全密码
-        this.password = password;
-      }
-    }
-  },
-  computed: {
-    option() {
-      return this.optionType ? '没有账户？点击注册' : '回到登录界面';
-    },
-    optionButton() {
-      return this.optionType ? '登录' : '注册';
-    }
-  },
-  methods: {
-    submit(event) {
-      if (!this.submitNumber) {
-        this.submitNumber += 1;
-      } else {
-        return;
-      }
-      let user = {
-        'username': this.name,
-        'password': this.password
+  export default {
+    data() {
+      return {
+        name: '',
+        password: '',
+        cPassword: '',
+        message: '',
+        regist: false,
+        optionType: OPTION_TYPE_REGSIT,
+        flag: true,
+        submitNumber: 0
       };
-      if (this.optionType) {
-        if (!(this.name && this.password)) {
-          this.submitNumber = 0;
-          return;
+    },
+    watch: {
+      // 监测name的值
+      name: function () {
+        this.password = '';
+        let password = loadFromLocal(this.name);
+        if (password) {
+          // 若本地缓存存在密码，则自动补全密码
+          this.password = password;
         }
-        // 保存密码到本地存储
-        saveToLocal(this.name, this.password);
-        // 发布登录事件
-        this.$socket.emit('login', user);
-      } else {
-        if (!(this.name && this.password && this.cPassword)) {
-          this.submitNumber = 0;
-          return;
+      }
+    },
+    beforeRouteEnter(to, from, next) {
+      next(vm => {
+        if (from.name && from.params.username) {
+          vm.signout(from.params.username);
         }
-        if (this.password !== this.cPassword) {
-          this.submitNumber = 0;
-          this.message = '两次输入的密码不一致，请确认';
+      });
+    },
+    computed: {
+      option() {
+        return this.optionType ? '没有账户？点击注册' : '回到登录界面';
+      },
+      optionButton() {
+        return this.optionType ? '登录' : '注册';
+      }
+    },
+    methods: {
+      signout(username) {
+        this.$socket.emit('iamOffline', { 'username': username });
+      },
+      submit(event) {
+        if (!this.submitNumber) {
+          this.submitNumber += 1;
         } else {
-          // 发布注册用户事件
-          this.$socket.emit('regist', user);
+          return;
+        }
+        let user = {
+          'username': this.name,
+          'password': this.password
+        };
+        if (this.optionType) {
+          if (!(this.name && this.password)) {
+            this.submitNumber = 0;
+            return;
+          }
+          // 保存密码到本地存储
+          saveToLocal(this.name, this.password);
+          // 发布登录事件
+          this.$socket.emit('login', user);
+        } else {
+          if (!(this.name && this.password && this.cPassword)) {
+            this.submitNumber = 0;
+            return;
+          }
+          if (this.password !== this.cPassword) {
+            this.submitNumber = 0;
+            this.message = '两次输入的密码不一致，请确认';
+          } else {
+            // 发布注册用户事件
+            this.$socket.emit('regist', user);
+          }
+        }
+      },
+      needRegist() {
+        this.name = '';
+        this.password = '';
+        this.cPassword = '';
+        this.regist = !this.regist;
+        this.optionType = !this.optionType;
+      },
+      needJump(status = false) {
+        this.flag = !status;
+        router.push(`room/${this.name}`);
+      }
+    },
+    sockets: {
+      loginMsg(data) {
+        this.submitNumber = 0;
+        data = JSON.parse(data);
+        if (!data.status) {
+          this.message = data.info;
+        } else {
+          this.message = '';
+          this.needJump(data.status);
+        }
+      },
+      registMsg(data) {
+        this.submitNumber = 0;
+        data = JSON.parse(data);
+        if (!data.status) {
+          this.message = data.info;
+        } else {
+          this.regist = false;
+          this.optionType = true;
+          this.message = '';
         }
       }
-    },
-    needRegist() {
-      this.name = '';
-      this.password = '';
-      this.cPassword = '';
-      this.regist = !this.regist;
-      this.optionType = !this.optionType;
-    },
-    needJump(status = false) {
-      this.flag = !status;
-      router.push(`room/${this.name}`);
     }
-  },
-  sockets: {
-    loginMsg(data) {
-      this.submitNumber = 0;
-      data = JSON.parse(data);
-      if (!data.status) {
-        this.message = data.info;
-      } else {
-        this.message = '';
-        this.needJump(data.status);
-      }
-    },
-    registMsg(data) {
-      this.submitNumber = 0;
-      data = JSON.parse(data);
-      if (!data.status) {
-        this.message = data.info;
-      } else {
-        this.regist = false;
-        this.optionType = true;
-        this.message = '';
-      }
-    }
-  }
-};
+  };
 </script>
 
 <style lang="less">
-.login {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  .logo-wrapper {
-    line-height: 50px;
-    width: 100%;
-    height: 110px;
-    text-align: center;
-    .title {
-      font-size: 50px;
-      font-weight: 700;
-      font-style: italic;
-      color: rgb(0, 160, 220);
-    }
-    .icon-chat {
-      font-size: 50px;
-    }
-  }
-  .login-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-sizing: border-box;
-    padding: 10px;
-    line-height: 16px;
-    width: 100%;
-    height: 50%;
-    font-size: 0px;
-    .name,
-    .password,
-    .confirm-password {
-      margin-bottom: 25px;
-      font-size: 16px;
-      label {
-        text-align: left;
-        padding: 3px;
-        display: inline-block;
-        width: 80px;
-      }
-      input {
-        padding: 3px;
-        border: 1px solid rgba(7, 17, 27, 0.1);
-        border-radius: 10px;
-        font-size: 16px;
-      }
-    }
-    .move-enter,
-    .move-leave-active {
-      opacity: 0;
-      transform: translateX(-100%);
-    }
-    .move-enter-active,
-    .move-leave-active {
-      transition: all 1s linear;
-    }
-    .login-select {
+  .login {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    .logo-wrapper {
+      line-height: 50px;
+      width: 100%;
+      height: 110px;
       text-align: center;
-      .confirm {
-        display: block;
-        padding: 2px 4px;
-        background: rgb(0, 160, 220);
-        border-radius: 10px;
-        line-height: 16px;
-        margin: 0 0 10px 33.5%;
-        width: 60%;
-        font-size: 16px;
-        color: #fff;
+      .title {
+        font-size: 50px;
+        font-weight: 700;
+        font-style: italic;
+        color: rgb(0, 160, 220);
       }
-      .regist {
-        margin: 0 0 10px 33.5%;
-        cursor: pointer;
-        font-size: 10px;
-        color: rgb(147, 153, 159);
+      .icon-chat {
+        font-size: 50px;
       }
     }
-    .message {
+    .login-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
       padding: 10px;
-      text-align: center;
-      .text {
-        margin-left: 33.5%;
-        line-height: 14px;
-        font-size: 12px;
-        color: rgb(240, 20, 20);
-        &.fade-enter {
-          opacity: 0;
+      line-height: 16px;
+      width: 100%;
+      height: 50%;
+      font-size: 0px;
+      .name,
+      .password,
+      .confirm-password {
+        margin-bottom: 25px;
+        font-size: 16px;
+        label {
+          text-align: left;
+          padding: 3px;
+          display: inline-block;
+          width: 80px;
         }
-        &.fade-enter-active {
-          transition: all .5s ease-in;
+        input {
+          padding: 3px;
+          border: 1px solid rgba(7, 17, 27, 0.1);
+          border-radius: 10px;
+          font-size: 16px;
+        }
+      }
+      .move-enter,
+      .move-leave-active {
+        opacity: 0;
+        transform: translateX(-100%);
+      }
+      .move-enter-active,
+      .move-leave-active {
+        transition: all 1s linear;
+      }
+      .login-select {
+        text-align: center;
+        .confirm {
+          display: block;
+          padding: 2px 4px;
+          background: rgb(0, 160, 220);
+          border-radius: 10px;
+          line-height: 16px;
+          margin: 0 0 10px 33.5%;
+          width: 60%;
+          font-size: 16px;
+          color: #fff;
+        }
+        .regist {
+          margin: 0 0 10px 33.5%;
+          cursor: pointer;
+          font-size: 10px;
+          color: rgb(147, 153, 159);
+        }
+      }
+      .message {
+        padding: 10px;
+        text-align: center;
+        .text {
+          margin-left: 33.5%;
+          line-height: 14px;
+          font-size: 12px;
+          color: rgb(240, 20, 20);
+          &.fade-enter {
+            opacity: 0;
+          }
+          &.fade-enter-active {
+            transition: all .5s ease-in;
+          }
         }
       }
     }
   }
-}
 </style>
